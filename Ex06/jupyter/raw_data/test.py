1fb00aa805d0bc0463979c0ebc0fef3c634097e6
rayza-piedade24 <rayza.janice5@outlook.com>
2024-10-30 19:29:55 +0000

    commit modified

--- Ex06/jupyter/raw_data/test.py
+++ Ex06/jupyter/raw_data/test.py
@@ -1,8 +1,9 @@
-import pandas as pdf
+import pandas as pd
 
-cancer_data = pd.read_csv('./raw-data/data.csv')
+cancer_data = pd.read_csv('data.csv')
 pd.options.display.max_columns = len(cancer_data)
 print(f'Number of entries: {cancer_data.shape[0]:,}\n'
       f'Number of features: {cancer_data.shape[1]:,}\n\n'
       f'Number of missing values: {cancer_data.isnull().sum().sum()}\n\n'
       f'{cancer_data.head(2)}')
+      
